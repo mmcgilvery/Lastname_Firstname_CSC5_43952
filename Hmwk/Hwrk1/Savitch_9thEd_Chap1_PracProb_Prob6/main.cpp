68b79058206019b5e438502391c3e98988145f4b
mmcgilvery <mmcgilvery@student.rcc.edu>
2015-03-08 12:30:25 -0700

    Signed off mmcgilvery <mmcgilvery@student.rcc.edu>

--- Hmwk/Hwrk1/Savitch_9thEd_Chap1_PracProb_Prob6/main.cpp
+++ Hmwk/Hwrk1/Savitch_9thEd_Chap1_PracProb_Prob6/main.cpp
@@ -3,7 +3,11 @@
  #6 from Savitch (Practice Problems); In class completion*/
 
 //System Libraries
-#include <iostream>
+#include < iostream 
+//a) Introduced extra space between "<" and iostream. 
+//Result: Cannot find system library, iostream.
+//b) Omitted one of the "<" in the include directive
+//Result: Cannot find include file.
 using namespace std;
 //User Libraries
 
@@ -13,21 +17,36 @@ using namespace std;
 //Function Prototypes
 
 //Execution begins here!
-int main( )    {
+ man  
+ //c) Omitted the int from the main function introduction.
+ //Result: Unresolved identifier - main
+ //d) Misspelled main
+ //Result: C++ requires an identifier
+ //e) Omitted one and both of the parentheses 
+ //Result: Unexpected token "(" and "Unable to resolve identifiers"
+ {
     //Declare Variables
     int lOfFnce, width, height;
     //Request data from user
-    cout << "Press return after entering a number." << endl;
+    cut << "Press return after entering a number." << endl;
+ //e1) Misspelled cout
+ //Result: Unexpected token "cut"
     cout << "Enter the width of the fence: " << endl;
-    cin >> width;
+    cn >> width;
+ //e2) Misspelled "cn"
+ //Result: Unexpected token "cut"
     cout << "Enter the height of the fence: " << endl;
-    cin >> height;
+    cin > height;
+ //e3) Omitted an ">" from ">>"
+ //Result: Unexpected token ">"
     //Make Calculation
     lOfFnce = width * height;
     //Output data
     cout << "If your fence's width is ";
     cout << width;
     cout << "," << endl;
+//e4) Misspelled cout
+//Result: Unexpected token "cut"
     cout << "and your fence's height is ";
     cout << height << ","<< endl;
     cout << "then your fence's total length is ";
@@ -35,4 +54,5 @@ int main( )    {
     cout << "Have a good day!" << endl;
     //Exit Stage left
     return 0;
-}
+//e5) Omiited the closing brace "}"
+//Result: Unexpected token "return 0"
