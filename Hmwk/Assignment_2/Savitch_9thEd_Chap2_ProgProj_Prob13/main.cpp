9e3f1979fb2af8c0fe1fb3a15dc668a6b682d189
mmcgilvery <mmcgilvery@student.rcc.edu>
2015-03-09 10:57:02 -0700

    signed off by matt

--- Hmwk/Assignment_2/Savitch_9thEd_Chap2_ProgProj_Prob13/main.cpp
+++ Hmwk/Assignment_2/Savitch_9thEd_Chap2_ProgProj_Prob13/main.cpp
@@ -1,6 +1,7 @@
 /* Matthew McGilvery
  3 - 9 - 15
- #13 from Savitch (Programming Projects) Chapter 2, 9th ED; In class completion*/
+ #13 from Savitch (Programming Projects) Chapter 2, 9th ED;
+ In class completion */
 
 //System Libraries
 #include <iostream>
@@ -8,17 +9,41 @@ using namespace std;
 //User Libraries
 
 
-//Global Constants : you  must add const before the variable
+//Global Constants
 
 //Function Prototypes
 
 //Execution begins here!
 int main( )    {
-    
-    
-    
-    
-    
+    //Declare variables
+    unsigned short lbs, hghtIn, ageInYr;
+    char mf;
+    unsigned char calCndy = 230;
+    float BMRFem, BMRMale, nCandy;
+    //Request input from user
+    cout << "How many candy bars can you consume whilst maintaining your weight?"<< endl;
+    cout << "We're about to find out!"<< endl;
+    cout << "Please enter your weight in pounds and press enter/return.";
+    cout << " Note: Please use integers!" << endl;
+    cin >> lbs;
+    cout << "Please enter your height in inches and press enter/return." << endl;
+    cin >> hghtIn;
+    cout << "Please enter your age in years and press enter/return." << endl;
+    cin >> ageInYr;
+    cout << "Are you male(m) or female(f)? Note: Use m or f"<< endl;
+    cin >> mf;
+    //Calculate
+    BMRFem = 655+4.3*lbs+4.7*hghtIn - 4.7*ageInYr ;
+    BMRMale = 66 + 6.3*lbs + 12.9*hghtIn - 6.8*ageInYr ;
+    //        Question    if Male           if Female
+    nCandy = ((mf=='m')?(BMRMale/calCndy): (BMRFem/calCndy));
+    //Output the results
+    cout << "Your weight = " << lbs << "(lbs)" << endl;
+    cout << "Your height = " << hghtIn << "(inches)" << endl;
+    cout << "Your age = " << ageInYr << "(years)" << endl;
+    cout << "Your sex = " <<((mf=='m')?"Male" : "Female" )<< endl;
+    cout << "You can eat " << nCandy << " candy bars per/day"<< endl;
+    //Exit stage right
     
     return 0;
 }
