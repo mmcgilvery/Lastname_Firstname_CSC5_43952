82b58082dfe9a6e39cbd7743fe6f29f8ad629022
mmcgilvery <mmcgilvery@student.rcc.edu>
2015-03-09 10:17:36 -0700

    signed off by matt

---
+++ Hmwk/Assignment_2/Savitch_9thEd_Chap2_ProgProj_Prob13/main.cpp
@@ -0,0 +1,24 @@
+/* Matthew McGilvery
+ 3 - 9 - 15
+ #13 from Savitch (Programming Projects) Chapter 2, 9th ED; In class completion*/
+
+//System Libraries
+#include <iostream>
+using namespace std;
+//User Libraries
+
+
+//Global Constants : you  must add const before the variable
+
+//Function Prototypes
+
+//Execution begins here!
+int main( )    {
+    
+    
+    
+    
+    
+    
+    return 0;
+}
